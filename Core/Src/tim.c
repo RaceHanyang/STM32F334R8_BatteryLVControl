4034f23e84aa8b328e170d36eb5b75565a3ad099
EluinLim <legolasism777@gmail.com>
2023-01-08 01:29:18 +0900

    BC-4 FIX : .ioc TIM1 prescaler
    prescaler 20-1 => 10-1
    Therefore, TIM1, TIM2, TIM3 PWM frequencies are all 25000Hz.

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
   * This software component is licensed by ST under BSD 3-Clause license,
@@ -47,7 +47,7 @@ void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 20-1;
+  htim1.Init.Prescaler = 10-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 288-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
